15981352507baaabd0440923028e1f80b422b82a
thomasadohle <dohle.t@husky.neu.edu>
2021-02-07 16:55:15 -0500

    compile war using mvn

---
+++ src/main/java/main.java
@@ -0,0 +1,9 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class main {
+
+    public static void main(String[] args){
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println(mapper);
+    }
+}
